4573ddf9cdb440e7ef45b99f88bfdf6f75f02835
JstLevi <levibaron143@gmail.com>
2025-09-11 19:29:13 +0800

    a

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,7 +2,7 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-  <Stack initialRouteName="login" screenOptions={{headerShown: false}}>
+  <Stack initialRouteName="index" screenOptions={{headerShown: false}}>
     <Stack.Screen name="index" />
     <Stack.Screen name="login" />
     <Stack.Screen name="sign_up" />
